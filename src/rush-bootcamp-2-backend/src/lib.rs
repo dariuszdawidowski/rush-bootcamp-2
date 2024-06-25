0cf49bc1810be49449556fc97290b9bc6b01aac4
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2024-06-25 19:12:30 +0200

    Records

--- src/rush-bootcamp-2-backend/src/lib.rs
+++ src/rush-bootcamp-2-backend/src/lib.rs
@@ -1,4 +1,28 @@
+use std::cell::RefCell;
+
+// Dane mają być przetwarzane lokalnie
+thread_local! {
+    static WPISY: RefCell<Vec<String>> = RefCell::default();
+}
+
+// Powitanie (query odczyt)
 #[ic_cdk::query]
 fn greet(name: String, last_name: String) -> String {
     format!("Hello, {} {}!", name, last_name)
+}
+
+// Zapis rekordu (update zapis)
+#[ic_cdk::update]
+fn dodaj_wpis(wpis: String) {
+    WPISY.with(|wpisy: &RefCell<Vec<String>>| {
+        return wpisy.borrow_mut().push(wpis);
+    });
+}
+
+// Listowanie rekordów
+#[ic_cdk::query]
+fn odczytaj_wpisy() -> Vec<String> {
+    return WPISY.with(|wpisy: &RefCell<Vec<String>>| {
+        return wpisy.borrow().clone();
+    });
 }
