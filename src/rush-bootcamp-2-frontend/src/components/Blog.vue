461b7bd3249a25a46aba1caf5c01f85c5e353a5d
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2024-06-25 19:39:58 +0200

    Adding entry

--- src/rush-bootcamp-2-frontend/src/components/Blog.vue
+++ src/rush-bootcamp-2-frontend/src/components/Blog.vue
@@ -1,3 +1,26 @@
 <template>
+    <button @click="pobierzWpisy">POBIERZ</button>
     <div>Hello Blog!</div>
+    {{ wpisy }}
 </template>
+
+<script>
+
+    import { rush_bootcamp_2_backend } from 'declarations/rush-bootcamp-2-backend/index';
+
+    export default {
+
+        data() {
+            return {
+                wpisy: []
+            };
+        },
+
+        methods: {
+            async pobierzWpisy() {
+                this.wpisy = await rush_bootcamp_2_backend.odczytaj_wpisy();
+            }
+        }
+
+    }
+</script>
