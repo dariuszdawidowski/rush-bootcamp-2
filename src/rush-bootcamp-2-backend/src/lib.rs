f07736a6b95adf4db1fef23216719490e03f05d9
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2024-06-26 17:38:02 +0200

    Usun_wpis and edytuj_wpis

--- src/rush-bootcamp-2-backend/src/lib.rs
+++ src/rush-bootcamp-2-backend/src/lib.rs
@@ -26,3 +26,22 @@ fn odczytaj_wpisy() -> Vec<String> {
         return wpisy.borrow().clone();
     });
 }
+
+// Kasowanie rekordów
+#[ic_cdk::update]
+fn usun_wpis(id_wpisu: usize) {
+    WPISY.with(|wpisy: &RefCell<Vec<String>>| {
+        wpisy.borrow_mut().remove(id_wpisu)
+    });
+}
+
+// Edycja rekordów
+#[ic_cdk::update]
+fn edytuj_wpis(id_wpisu: usize, nowy_wpis: String) {
+    WPISY.with(|wpisy| {
+        let mut binding = wpisy.borrow_mut();
+        let wpis = binding.get_mut(id_wpisu);
+        let stary_wpis = wpis.unwrap();
+        *stary_wpis = nowy_wpis;
+    });
+}
