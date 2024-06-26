b283dab31f0ca29e7c3d56f0c6e5dd26caf20f05
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2024-06-26 19:06:10 +0200

    Styling

--- src/rush-bootcamp-2-frontend/src/App.vue
+++ src/rush-bootcamp-2-frontend/src/App.vue
@@ -1,33 +1,12 @@
 <script setup>
-import { ref } from 'vue';
-import { rush_bootcamp_2_backend } from 'declarations/rush-bootcamp-2-backend/index';
-import Blog from './components/Blog.vue';
-let greeting = ref('');
-
-async function handleSubmit(e) {
-  e.preventDefault();
-  const target = e.target;
-  const name = target.querySelector('#name').value;
-  const last_name = target.querySelector('#last_name').value;
-  await rush_bootcamp_2_backend.greet(name, last_name).then((response) => {
-    greeting.value = response;
-  });
-}
+    import Blog from './components/Blog.vue';
 </script>
 
 <template>
-  <main>
-    <img src="/logo2.svg" alt="DFINITY logo" />
-    <br />
-    <br />
-    <h1 class="text-blue-600">Hello</h1>
-    <form action="#" @submit="handleSubmit">
-      <label for="name">Enter your name: &nbsp;</label>
-      <input id="name" alt="Name" type="text" />
-      <input id="last_name" alt="Last Name" type="text" />
-      <button type="submit">Click Me!</button>
-    </form>
-    <section id="greeting">{{ greeting }}</section>
-    <Blog />
-  </main>
+    <main class="container mx-auto">
+        <img src="/logo2.svg" alt="DFINITY logo" />
+        <br />
+        <br />
+        <Blog />
+    </main>
 </template>
--- src/rush-bootcamp-2-frontend/src/components/Blog.vue
+++ src/rush-bootcamp-2-frontend/src/components/Blog.vue
@@ -1,9 +1,15 @@
 <template>
-    <button @click="pobierzWpisy">POBIERZ</button>
-    <div>Hello Blog!</div>
-    {{ wpisy }}
-    <input v-model="nowyBlog" type="text">
-    <button @click="dodajWpis">DODAJ</button>
+    <h2 class="text-blue-600">Wpisy</h2>
+    <div class="w-100 flex flex-row-reverse">
+        <button @click="pobierzWpisy" class="bg-blue-600 rounded text-white p-4">POBIERZ</button>
+    </div>
+    <div class="grid mx-6 gap-4 my-4">
+        <div v-for="wpis in wpisy" class="drop-shadow-xl bg-stone-300 p-4">
+            <p>{{ wpis }}</p>
+        </div>
+    </div>
+    <input v-model="nowyBlog" type="text" class="border-1 border-blue-600 p-4">
+    <button @click="dodajWpis" class="bg-blue-600 rounded text-white p-4">DODAJ</button>
 </template>
 
 <script>
@@ -27,6 +33,10 @@
             async pobierzWpisy() {
                 this.wpisy = await rush_bootcamp_2_backend.odczytaj_wpisy();
             }
+        },
+
+        async mounted() {
+            this.pobierzWpisy();
         }
 
     }
